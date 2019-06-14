6d03660a62cbe081985bb20b7a0879a9a86980e7
dmdylan <dylandanowski@gmail.com>
2019-06-13 20:16:48 -0400

    Starting progress

---
+++ AdventureRPG_Classes/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureRPG_Classes
+{
+    public class Player
+    {
+        //private int health;
+        //private int armor;
+        //private int attackDamage;
+        //public string name;
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+
+    }
+    
+}
---
+++ AdventureRPG_Classes/ViewModels/GameSession.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureRPG_Classes;
+
+namespace AdventureRPG_Classes.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Dylan";
+        }
+    }
+}
+
+/*
+using System;
+using AdventureRPG_Classes;
+
+namespace AdventureRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameStart();
+            Console.WriteLine("done");
+            
+            Console.ReadLine();
+        }
+
+        private static void GameStart()
+        {
+            Console.WriteLine("Enter 1 to start a new game or 2 to load a saved game!");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    StartANewGame();
+                    break;
+                case "2":
+                    LoadGame();
+                    break;
+                default:
+                    Console.WriteLine("Please enter 1 or 2");
+                    break;
+            }
+        }
+
+        public static void StartANewGame()
+        {
+            Console.WriteLine("Please enter a character name:");
+            string playerName = Console.ReadLine();
+            if (string.IsNullOrEmpty(playerName) || IsInputAString())
+            {
+                Console.WriteLine("Name cannot be empty, please enter a name for your character!");
+            }
+            Player player = new Player(playerName);
+        }
+
+        private static void LoadGame()
+        {
+
+        }
+
+        private static void SaveGame()
+        {
+
+        }
+
+        private static bool IsInputAString()
+        {
+            var input = Console.ReadLine();
+            var isNumeric = int.TryParse(input, out int n);
+
+            if (isNumeric)
+            {
+                Console.WriteLine("Please only use letters!");
+            }
+
+            return true;
+        }
+    }
+}
+
+ */
