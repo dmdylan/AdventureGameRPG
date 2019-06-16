ab82eecd9e43d7d44d2c19da45ada6f1a1a63b7f
dmdylan <dylandanowski@gmail.com>
2019-06-16 19:50:49 -0400

    player inventory functionality and base item/weapon class

---
+++ AdventureRPG_Classes/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureRPG_Classes
+{
+    public class Item
+    {
+        public int Value { get; set; }
+        public int ItemType { get; set; }
+        public string Name { get; set; }
+
+        public Item(string name, int itemType, int value)
+        {
+            Value = value;
+            ItemType = itemType;
+            Name = name;
+        }
+    }
+}
---
+++ AdventureRPG_Classes/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureRPG_Classes
+{
+    class Weapon : Item
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(string name, int itemType, int value, int minimumDamage, int maximumDamage):base(name, itemType, value)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+    }
+}
